20510bd3b749fa0499bb14f8f1d40798c1110b83
Jack Williams <jackawilliams13@gmail.com>
2024-11-27 22:30:05 -0600

    be cutsie

--- app/components/story.tsx
+++ app/components/story.tsx
@@ -5,5 +5,16 @@ return <section id="story" className="pt-5">
 <p className="indent-8">After college, we packed up and moved across the country to Madison, WI in January 2022. Jack is working as a software developer at Epic Systems and Hannah is finishing her Master’s degree in environmental engineering in December. We absolutely love our life in Madison with our two cats, Pesto and Moka, and can't wait to start the next chapter!</p>
 <p className="indent-8">Jack proposed to Hannah in November 2023. He said to get ready so that we could take fall photos in the UW Arboretum down the street from our house. As we were walking through the Arboretum, he proposed in the same area where we had our first date in Madison when Hannah was visiting him when he was an intern at Epic in Summer 2020!</p>
 
+<h3 className="text-xl pt-2 text-[#879b88]">What's your favorite activity together?</h3>
+<p>We both love biking, cooking, gardening, and spoiling our cats, Pesto and Moka! Hannah humors Jack by playing board games, and Jack humors Hannah by going on long walks in our neighborhood.</p>
+
+
+<h3 className="text-xl pt-2 text-[#879b88]">Honeymoon destination?</h3>
+<p>Greece! We had planned a trip to Greece in January 2022 right before moving to Madison, but we unfortunately got COVID right before we were supposed to leave. We're so excited we finally get to take this trip and think it will be a perfect mix of nature, history, and relaxation, not to mention the amazing food!</p>
+
+<h3 className="text-xl pt-2 text-[#879b88]">Most memorable trip?</h3>
+<p>Our first study abroad trip in Oxford will always be so special to us, especially the weekend we spent in the Lake District hiking with a group of our friends. Our favorite trip we've taken just the two of us was when we went to Indianapolis in April 2024 to see the total solar eclipse! It was absolutely life-changing and we definitely can see ourselves becoming lifelong eclipse chasers.</p>
+
+
 </section>
 }
