057d837da0123e2cbde64a296e5a641fbcf95533
Jack Williams <32182937+will-jac@users.noreply.github.com>
2025-02-22 14:11:22 -0600

    Update page.tsx

--- app/registry/page.tsx
+++ app/registry/page.tsx
@@ -4,6 +4,6 @@ import HomeLayout from '../components/HomeLayout';
 export default function registry() {
     return <HomeLayout headerImageSrc="Hannah-Jack-ENG-AKP-6.17.24-59.jpg">
         <h1>Registry</h1>
-        <p>Registry coming soon!!</p>
+        We are registered at <a href="https://www.williams-sonoma.com/registry/zb68pf9tr7/registry-list.html">Williams Sonoma</a>
     </HomeLayout>
 }
