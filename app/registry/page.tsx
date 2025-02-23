95a0469bf6d682173e199a3cf4c4024f02ccedfc
Jack Williams <jackawilliams13@gmail.com>
2025-02-22 20:44:45 -0600

    reword registry

--- app/registry/page.tsx
+++ app/registry/page.tsx
@@ -4,12 +4,13 @@ import HomeLayout from '../components/HomeLayout';
 
 export default function registry() {
     return <HomeLayout>
-        <h1>Registry</h1>
-        <p>We have registered at <Link href="https://www.williams-sonoma.com/registry/zb68pf9tr7/registry-list.html">Williams Sonoma</Link></p>
+        <h1 className="text-2xl font-extrabold text-[#879b88]">Registry</h1>
         <br/>
-        <p>You can also help contribute to our <Link href="https://account.venmo.com/u/hannah-curt">honeymoon fund!</Link></p>
+        <p>We are registered at <Link href="https://www.williams-sonoma.com/registry/zb68pf9tr7/registry-list.html">Williams Sonoma.</Link></p>
         <br/>
-        <p>For our honeymoon, we are going to Greece! We originally planned to go to Greece three years ago, but unfortunately were not able to go. We've been wanting to go ever since, and are excited to get this opportunity to!</p>
+        <p>You can also help <Link href="https://account.venmo.com/u/hannah-curt">contribute</Link> to our honeymoon fund, or just support us in the next phase of our life.</p>
+        <br/>
+        <p>For our honeymoon, we are going to Greece! We originally planned to go to Greece three years ago, but unfortunately were not able to go. We've been wanting to go ever since, and are excited to get this opportunity to.</p>
         <p>We'll be flying into Athens, then going to the islands of Naxos and Santorini.</p>
     </HomeLayout>
 }
