d2b8ff3dcdc206966ddfbe5458944b76a6631816
Jack Williams <jackawilliams13@gmail.com>
2025-02-22 20:20:15 -0600

    add registry info

--- app/registry/page.tsx
+++ app/registry/page.tsx
@@ -1,9 +1,15 @@
 'use client';
+import { Link } from '@mui/material';
 import HomeLayout from '../components/HomeLayout';
 
 export default function registry() {
-    return <HomeLayout headerImageSrc="Hannah-Jack-ENG-AKP-6.17.24-59.jpg">
+    return <HomeLayout>
         <h1>Registry</h1>
-        We are registered at <a href="https://www.williams-sonoma.com/registry/zb68pf9tr7/registry-list.html">Williams Sonoma</a>
+        <p>We have registered at <Link href="https://www.williams-sonoma.com/registry/zb68pf9tr7/registry-list.html">Williams Sonoma</Link></p>
+        <br/>
+        <p>You can also help contribute to our <Link href="https://account.venmo.com/u/hannah-curt">honeymoon fund!</Link></p>
+        <br/>
+        <p>For our honeymoon, we are going to Greece! We originally planned to go to Greece three years ago, but unfortunately were not able to go. We've been wanting to go ever since, and are excited to get this opportunity to!</p>
+        <p>We'll be flying into Athens, then going to the islands of Naxos and Santorini.</p>
     </HomeLayout>
 }
