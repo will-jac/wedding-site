022c5e39ff4a57bb4bdaad53fb5d796ed2b4b0e3
Nafis Zaman <nafzam@outlook.com>
2025-05-30 04:57:33 +0000

    add key property to fragments

--- app/solutions/page.tsx
+++ app/solutions/page.tsx
@@ -7,13 +7,14 @@ import solutionMini from "../images/solution_mini.png";
 import solutionConnections from "../images/solution_connections.png";
 import solutionBee from "../images/solution_bee.png";
 import solutionStrands from "../images/solution_strands.png";
+import { Fragment } from 'react';
 
 interface PuzzleSolution {
     title: string;
     image: StaticImageData;
 }
 
-export default function Travel() {
+export default function Solutions() {
 
     const puzzleSolutions: PuzzleSolution[] = [
         {
@@ -37,7 +38,7 @@ export default function Travel() {
     return (
         <HomeLayout headerImageSrc="engagement/Hannah-Jack-ENG-AKP-6.17.24-196.jpg">
             {puzzleSolutions.map(s => (
-                <>
+                <Fragment key={s.title}>
                     <h1 className="text-2xl font-extrabold text-[#879b88]">{s.title}</h1>
                     <Image
                         src={s.image}
@@ -46,7 +47,7 @@ export default function Travel() {
                         className="object-cover pb-10"
                         priority
                     />
-                </>
+                </Fragment>
             ))}
         </HomeLayout>
     );
