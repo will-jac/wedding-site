ee21bf04749558ccba631e7c2d09def8e5d77a5f
Nafis Zaman <nafzam@outlook.com>
2025-05-30 04:52:25 +0000

    add puzzle solutions to solutions page

--- app/solutions/page.tsx
+++ app/solutions/page.tsx
@@ -1,10 +1,53 @@
 'use client';
 
 import HomeLayout from '../components/HomeLayout';
+import Image, { StaticImageData } from "next/image";
+
+import solutionMini from "../images/solution_mini.png";
+import solutionConnections from "../images/solution_connections.png";
+import solutionBee from "../images/solution_bee.png";
+import solutionStrands from "../images/solution_strands.png";
+
+interface PuzzleSolution {
+    title: string;
+    image: StaticImageData;
+}
 
 export default function Travel() {
 
-    return <HomeLayout headerImageSrc="engagement/Hannah-Jack-ENG-AKP-6.17.24-196.jpg">
-        <h1>Answers go here</h1>
-    </HomeLayout>;
+    const puzzleSolutions: PuzzleSolution[] = [
+        {
+            title: 'Down the Aisle',
+            image: solutionMini,
+        },
+        {
+            title: 'Love Connections',
+            image: solutionConnections,
+        },
+        {
+            title: 'Wedding Bee',
+            image: solutionBee,
+        },
+        {
+            title: 'Heart Strands',
+            image: solutionStrands,
+        },
+    ];
+
+    return (
+        <HomeLayout headerImageSrc="engagement/Hannah-Jack-ENG-AKP-6.17.24-196.jpg">
+            {puzzleSolutions.map(s => (
+                <>
+                    <h1 className="text-2xl font-extrabold text-[#879b88]">{s.title}</h1>
+                    <Image
+                        src={s.image}
+                        width={500}
+                        alt={`Solution for the ${s.title} puzzle`}
+                        className="object-cover pb-10"
+                        priority
+                    />
+                </>
+            ))}
+        </HomeLayout>
+    );
 }
